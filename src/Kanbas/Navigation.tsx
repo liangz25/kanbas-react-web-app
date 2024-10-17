6672abb1789eb88c76821a79c10802ed45cc1f13
liangzhen <liang.zhen@northeastern.edu>
2024-10-17 09:34:58 -0700

    A3

--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { AiOutlineDashboard } from "react-icons/ai";
 import { IoCalendarOutline } from "react-icons/io5";
 import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
@@ -6,7 +6,17 @@ import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
 import { HiInboxArrowDown } from "react-icons/hi2";
 import { FaCalendarAlt } from "react-icons/fa";
 import { ImLab } from "react-icons/im";
+
 export default function KanbasNavigation() {
+    const { pathname } = useLocation();
+    const links = [
+        { label: "Dashboard", path: "/Kanbas/Dashboard", icon: AiOutlineDashboard },
+        { label: "Courses", path: "/Kanbas/Dashboard", icon: LiaBookSolid },
+        { label: "Calendar", path: "/Kanbas/Calendar", icon: IoCalendarOutline },
+        { label: "Inbox", path: "/Kanbas/Inbox", icon: FaInbox },
+        { label: "Labs", path: "/Labs", icon: LiaCogSolid },
+    ];
+
     return (
         <div id="wd-kanbas-navigation" style={{ width: 120 }} className="list-group rounded-0 position-fixed
         position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2">
@@ -37,6 +47,20 @@ export default function KanbasNavigation() {
                    bg-black text-center border-0"><HiInboxArrowDown className="fs-1 text-danger" /><br />Inbox</Link>
             <Link to="/Labs" id="wd-labs-link" className="list-group-item text-white
                    bg-black text-center border-0"><ImLab className="fs-1 text-danger" /><br />Labs</Link>
+            <Link to="/Kanbas/Account" className={`list-group-item text-center border-0 bg-black
+            ${pathname.includes("Account") ? "bg-white text-danger" : "bg-black text-white"}`}>
+                <FaRegCircleUser className={`fs-1 ${pathname.includes("Account") ? "text-danger" : "text-white"}`} />
+                <br />
+                Account
+            </Link>
+            {links.map((link) => (
+                <Link key={link.path} to={link.path} className={`list-group-item bg-black text-center border-0
+              ${pathname.includes(link.label) ? "text-danger bg-white" : "text-white bg-black"}`}>
+                    {link.icon({ className: "fs-1 text-danger" })}
+                    <br />
+                    {link.label}
+                </Link>
+            ))}
         </div>
     );
 }
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router";
 import Lab1 from "./Lab1"
 import Lab2 from "./Lab2";
+import Lab3 from "./Lab3";
 import TOC from "./TOC";
 export default function Labs() {
     return (
@@ -13,7 +14,8 @@ export default function Labs() {
             <Routes>
                 <Route path = "Lab1" element ={<Lab1 />}/>
                 <Route path = "Lab2" element ={<Lab2 />}/>
-                <Route path = "Lab2" element = {<h2>lab3</h2>}/>
+                {/* <Route path = "Lab2" element = {<h2>lab3</h2>}/> */}
+                <Route path="Lab3/*" element={<Lab3 />} />
             </Routes>
         </div>
     );
