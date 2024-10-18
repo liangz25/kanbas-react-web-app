cffcc877b0aee726929521cf1c4f8d608d77e05b
liangzhen <liang.zhen@northeastern.edu>
2024-10-17 17:01:36 -0700

    A3Names

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -6,7 +6,7 @@ import TOC from "./TOC";
 export default function Labs() {
     return (
         <div>
-            <h1>Liang Zhen</h1>
+            <h1>Zhen Liang</h1>
             <h1>section2</h1>
             <h1>https://github.com/liangz25/kanbas-react-web-app.git</h1>
             <h1>Labs</h1>
