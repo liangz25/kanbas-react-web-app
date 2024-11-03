a8b80c335786bf10e47469f928a8457412e06761
liangzhen <liang.zhen@northeastern.edu>
2024-11-03 08:06:39 -0800

    before editing course

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -10,7 +10,7 @@ export default function Dashboard(
         updateCourse: () => void; }
 ) {
     
-    
+    const [setCourses] = useState<any[]>(db.courses);
     const { currentUser } = useSelector((state: any) => state.accountReducer);
     const { enrollments } = db;
 
@@ -22,8 +22,8 @@ export default function Dashboard(
                     id="wd-add-new-course-click"
                     onClick={addNewCourse} > Add </button>
             </h5><br />
-            <input defaultValue={course.name} className="form-control mb-2" onChange={(e) => setCourse({ ...course, name: e.target.value })} />
-            <textarea defaultValue={course.description} className="form-control" onChange={(e) => setCourse({ ...course, description: e.target.value })} /><hr />
+            <input value={course.name} className="form-control mb-2" onChange={(e) => setCourse({ ...course, name: e.target.value })} />
+            <textarea value={course.description} className="form-control" onChange={(e) => setCourse({ ...course, description: e.target.value })} /><hr />
 
             <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
             <div id="wd-dashboard-courses" className="row">
@@ -35,8 +35,8 @@ export default function Dashboard(
                             enrollment.user === currentUser._id &&
                             enrollment.course === course._id
                            ))                  
-                    .map((course:any) => (
-                        <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+                    .map((course) => (
+                        <div key={course._id} className="wd-dashboard-course col" style={{ width: "300px" }}>
                             <div className="card rounded-3 overflow-hidden">
                                 <Link className="wd-dashboard-course-link text-decoration-none text-dark"
                                     to="/Kanbas/Courses/${1234}/Home">
